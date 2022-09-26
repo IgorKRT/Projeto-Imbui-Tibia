7f43c39e3a859f87053d8535fffd572539396359
IgorKRT <c.igor@live.com>
2022-09-26 18:25:45 -0300

    atts gerais

---
+++ scripts/calc.js
@@ -0,0 +1,15 @@
+var nvOne = document.querySelector("#vn1");
+var precoOne = parseInt(nvOne)*25;
+
+var nvTwo = document.querySelector("#vn2");
+var precoTwo = parseInt(nvTwo)*15;
+
+var nvTri = document.querySelector("#vn3");
+var precoTri = parseInt(nvTri)*5;
+
+var botaoCacl =document.querySelector("#btnclc");
+ botaoCacl.addEventListener('click', function(event){
+    event.preventDefault;
+    
+ })
+
---
+++ scripts/visibilidade.js
@@ -0,0 +1,93 @@
+var equipImbui = document.querySelector("#i-item");
+
+equipImbui.addEventListener('input',function(event){
+    event.preventDefault;
+    var item = cadastraItem(equipImbui);
+    if (item=="Espada" || item=="Clava" || item=="Axe"){
+        var disableMe = document.querySelector("#imbui-melee");
+        disableMe.classList.remove("invisivel");
+    };
+    if (item=="Cajado"){
+        var disableCa = document.querySelector("#imbui-cajado");
+        disableCa.classList.remove("invisivel");
+    };
+    if (item=="Arco/Besta"){
+        var disableDis = document.querySelector("#imbui-distancia");
+        disableDis.classList.remove("invisivel");
+    };
+    if (item=="Capacete"){
+        var disableCapa = document.querySelector("#imbui-capacete");
+        disableCapa.classList.remove("invisivel");
+    };
+    if (item=="Bota"){
+        var disableBo = document.querySelector("#imbui-bota");
+        disableBo.classList.remove("invisivel");
+    };
+    if (item=="Armadura"){
+        var disableAr = document.querySelector("#imbui-armadura");
+        disableAr.classList.remove("invisivel");
+    };
+    if (item=="Escudo"){
+        var disableEs = document.querySelector("#imbui-escudo");
+        disableEs.classList.remove("invisivel");
+    };
+    if (item=="Backpack"){
+        var disableBa = document.querySelector("#imbui-backpack");
+        disableBa.classList.remove("invisivel");
+    };
+
+    if (item==""){
+        var disableAll = document.querySelector("#imbui-cajado");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-melee");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-distancia");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-capacete");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-bota");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-armadura");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-escudo");
+        disableAll.classList.add("invisivel");
+        var disableAll = document.querySelector("#imbui-backpack");
+        disableAll.classList.add("invisivel");
+    }
+var meleeInput = document.querySelector("#i-melee");
+meleeInput.addEventListener('input',function(event){
+    event.preventDefault;
+    var melee = cadastraItem(meleeInput);
+    console.log(melee);
+    if (melee=="Life Leech"){
+        var disableLi = document.querySelector("#imbui-life-leech");
+        disableLi.classList.remove("invisivel")
+    }
+})
+var tierImbui = document.querySelector("#imbui-life-leech");
+tierImbui.addEventListener('input', function(event){
+    event.preventDefault;
+    var disableVn1 = document.querySelector("#vn1")
+    disableVn1.classList.remove("invisivel")
+})
+
+
+
+})
+
+
+var slots = document.querySelector("#i-slot");
+
+slots.addEventListener('input',function(event){
+    event.preventDefault;
+    var slot = cadastraItem(slots);
+    console.log(slot)
+    if (slot=="1 Slot"){console.log("slot um")}
+    if (slot=="2 Slots"){console.log("slot dois")}
+    if (slot=="3 Slots"){console.log("slot tres")}
+})
+
+function cadastraItem(valor){
+    var selecionado = valor.value;
+    return selecionado;
+}
